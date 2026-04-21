2d1fc39806eaae85733eefa9ef5a5fd2c926a413
Poorna Nag <poornanag@Poornas-MacBook-Air.local>
2026-04-21 19:08:22 +0530

    Add initial implementation of AuthRepo and UserModel with JSON serialization support

---
+++ lib/features/auth/data/user_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$$UserModelImpl _$$$UserModelImplFromJson(Map<String, dynamic> json) =>
+    _$$UserModelImpl(
+      email: json['email'] as String,
+      passcode: json['passcode'] as String,
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$$$UserModelImplToJson(_$$UserModelImpl instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'passcode': instance.passcode,
+      'name': instance.name,
+    };
